c39fa5d3b6cd998468308eac854423fbf551e385
= <=>
2018-04-20 17:53:09 -0700

    set up endpoint for parse and grabbed HTML

--- express-server.js
+++ express-server.js
@@ -6,3 +6,23 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/parse', (req, res) => {
+  const site = req.query.endpoint;
+  const tagName = req.query.tag;
+  let html;
+  axios.get(`http://${site}/`)
+    .then(siteData => {
+      html = siteData.data;
+      console.log(html)
+      return html;
+    });
+  
+//  const parser = new DOMParser();
+//  let doc = parser.parseFromString(html, "text/html");
+  res.end("hello");
+});
+
+app.listen(8080, () => {
+  console.log(' ʕ´• ᴥ •`ʔ listening on port 8080 ʕ´• ᴥ •`ʔ ');
+});
